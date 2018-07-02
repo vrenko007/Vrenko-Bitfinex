2f7c26ea0c899cdfa792cda52c102b026c1a6175
Matic Vrenko <matic.vrenko@gmail.com>
2018-07-02 04:04:16 +0200

    Python test spremenjen v SocketIO Klient
    SocketIO = komunikacija med frontend in backend

--- PythonTest/Main.py
+++ PythonTest/Main.py
@@ -1,44 +1,25 @@
-import websocket
-try:
-    import thread
-except ImportError:
-    import _thread as thread
-import time
-
-from json import JSONDecoder
-
-id = 0
-
-def on_message(ws, message):
-    print(message)
-    array = JSONDecoder().decode(message)
-    if type(array) is dict:
-        print(array["chanId"])
-        id = array["chanId"]
-        def run(*args):
-            for i in range(10):
-                time.sleep(1)
-            ws.send('{"event":"unsubscribe","chanId":"'+str(args[0])+'"}')
-            time.sleep(1)
-            ws.close()
-            print("thread terminating...")
-        thread.start_new_thread(run, (id))
-
-def on_error(ws, error):
-    print(error)
-
-def on_close(ws):
-    print("### closed ###")
-
-def on_open(ws):
-    ws.send('{"event":"subscribe","channel":"trades","pair":"BTCUSD"}')
+from socketIO_client import SocketIO
 
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://api.bitfinex.com/ws",
-                              on_message = on_message,
-                              on_error = on_error,
-                              on_close = on_close)
-    ws.on_open = on_open
-    ws.run_forever()
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_response(*args):
+    print(args[0])
+
+socketIO = SocketIO('127.0.0.1', 5000, verify=False)
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
+# Listen
+socketIO.on('transaction', on_response)
+
+socketIO.emit('join',{ "room":"BTCUSD" })
+
+socketIO.wait(seconds=15)
